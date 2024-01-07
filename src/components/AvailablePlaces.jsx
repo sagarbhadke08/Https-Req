3e72658f431f5364e20826bf23d341f770544d9a
sagarbhadke08 <sagarbhadke08@gmail.com>
2024-01-07 20:58:36 +0530

    updated after the Added Error component in the code

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,22 +1,57 @@
 import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
+import Error from './Error.jsx';
 
-// const places = localStorage.getItem();
+import { sortPlacesByDistance } from '../loc.js';
+
 export default function AvailablePlaces({ onSelectPlace }) {
 
   const [isFeatching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
+
 
   useEffect(() => {
 
     async function fetchPlaces() {
       setIsFetching(true);
-      const response = await fetch('http://localhost:3000/places');
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
-      setIsFetching(false);
+
+      try {
+        const response = await fetch('http://localhost:3000/places');
+        const resData = await response.json();
+
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch the places');
+        }
+
+        navigator.geolocation.getCurrentPosition(() => {
+
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+
+          setAvailablePlaces(resData.places);
+          setIsFetching(false);
+        })
+
+      } catch (error) {
+        setError({
+          message:
+            error.message || 'Could not fetch places, Please try again later.'
+        });
+      }
+
+
     }
+    fetchPlaces();
   }, []);
+
+  if (error) {
+    return <Error title="An error occurred!" message={error.message} />;
+  }
 
   return (
     <Places
@@ -24,7 +59,7 @@ export default function AvailablePlaces({ onSelectPlace }) {
       places={availablePlaces}
       fallbackText="No places available."
       loadingText='Featching places data'
-      isLoading={true}
+      isLoading={isFeatching}
       onSelectPlace={onSelectPlace}
     />
   );
