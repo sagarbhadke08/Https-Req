c9fb38d117c94516531220ee80f750cc7f5608fc
sagarbhadke08 <sagarbhadke08@gmail.com>
2024-01-08 23:09:39 +0530

    updated

--- src/http.js
+++ src/http.js
@@ -1,5 +1,4 @@
 export async function fetchAvailablePlaces() {
-
   const response = await fetch("http://localhost:3000/places");
   const resData = await response.json();
 
@@ -10,23 +9,20 @@ export async function fetchAvailablePlaces() {
   return resData.places;
 }
 
-async function updateUserPlaces(places) {
+export async function updateUserPlaces(places) {
   const response = await fetch("http://localhost:3000/user-places", {
     method: "PUT",
-    body: JSON.stringify(places),
+    body: JSON.stringify({places}),
     headers: {
       "Content-Type": "application/json",
     },
   });
 
-  
   const resData = await response.json();
 
-
-  if(!response.ok){
-    throw new Error('Failed to update user data.');
+  if (!response.ok) {
+    throw new Error("Failed to update user data.");
   }
 
-
+  return resData.message;
 }
-
